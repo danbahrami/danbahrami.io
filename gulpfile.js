d682a340932862be143fbe1c46917ef66d3f31d0
danbahrami <dan.bahrami@googlemail.com>
2016-02-27 19:27:39 +0100

    Gulp now hashing css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 var gulp         = require("gulp"),
     sourcemaps   = require("gulp-sourcemaps"),
     sass         = require("gulp-sass"),
-    autoprefixer = require("gulp-autoprefixer")
+    autoprefixer = require("gulp-autoprefixer"),
+    hash         = require("gulp-hash"),
+    del          = require("del")
 
 // Compile SCSS files to CSS
 gulp.task("scss", function () {
+    del(["themes/danesis/static/css/**/*"])
     gulp.src("scss/**/*.scss")
         .pipe(sourcemaps.init())
         .pipe(sass({errLogToConsole : true}))
         .pipe(autoprefixer({browsers : ["last 20 versions"]}))
         .pipe(sourcemaps.write())
+        .pipe(hash())
         .pipe(gulp.dest("themes/danesis/static/css"))
+        .pipe(hash.manifest("hash-manifest.json"))
+        .pipe(gulp.dest("public"))
 })
 
 // Watch SCSS folder for changes
