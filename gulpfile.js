669ab20600d27882fe694f4e645c21d3608d479c
danbahrami <dan.bahrami@googlemail.com>
2016-03-13 10:37:51 +0000

    Separated gulp tasks into dev and prod to remove source maps from prod build

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,24 @@ var gulp         = require("gulp"),
     hash         = require("gulp-hash"),
     del          = require("del")
 
-// Compile SCSS files to CSS
+//Compile SCSS files to CSS
 gulp.task("scss", function () {
     del(["static/css/**/*"])
     gulp.src("src/scss/**/*.scss")
+        .pipe(sass({outputStyle : "compressed"}))
+        .pipe(autoprefixer({browsers : ["last 20 versions"]}))
+        .pipe(hash())
+        .pipe(gulp.dest("static/css"))
+        .pipe(hash.manifest("hash.json"))
+        .pipe(gulp.dest("data/styles"))
+})
+
+// Compile SCSS files to CSS with source maps
+gulp.task("dev-scss", function () {
+    del(["static/css/**/*"])
+    gulp.src("src/scss/**/*.scss")
         .pipe(sourcemaps.init())
-        .pipe(sass({
-            errLogToConsole : true,
-            outputStyle     : "compressed"
-        }))
+        .pipe(sass({errLogToConsole : true}))
         .pipe(autoprefixer({browsers : ["last 20 versions"]}))
         .pipe(sourcemaps.write())
         .pipe(hash())
@@ -22,6 +31,7 @@ gulp.task("scss", function () {
         .pipe(gulp.dest("data/styles"))
 })
 
+// Hash images
 gulp.task("images", function () {
     del(["static/images/**/*"])
     gulp.src("src/images/**/*")
@@ -32,10 +42,10 @@ gulp.task("images", function () {
 })
 
 // Watch asset folder for changes
-gulp.task("watch", ["scss", "images"], function () {
+gulp.task("watch", ["dev-scss", "images"], function () {
     gulp.watch("src/scss/**/*", ["scss"])
     gulp.watch("src/images/**/*", ["images"])
 })
 
-// Set SCSS compiling as default task
-gulp.task("default", ["watch"])
+// Set production build as default task
+gulp.task("default", ["scss", "images"])
